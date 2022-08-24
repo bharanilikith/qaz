cdde7af1765c9bb6b8af0923eb47d545e26d278d
Bharanilikith <Bharanilikith@Bharani>
2022-08-24 14:23:02 +0530

    hi

---
+++ src/test/java/jenkins/Test2.java
@@ -0,0 +1,15 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+
+@Test
+public void tc1(){
+
+System.out.println("Om ganesha");
+System.out.println("om shiva");
+
+	}
+
+}
